33deeb6ea4e668f36a7b988c1c83526a69a7927f
Rafael Gieschke <rafael@gieschke.de>
2023-04-18 16:20:58 +0200

    feat: add script to create browser environment

---
+++ create-browser-environment.js
@@ -0,0 +1,25 @@
+#!/bin/sh
+//bin/sh -c : && exec deno run -A "$0" "$@"
+
+import { clientFromUrl } from "./lib.js";
+
+let [instanceUrl, label, url] = Deno.args;
+
+const client = await clientFromUrl(instanceUrl);
+url = String(new URL(encodeURI(url)));
+
+console.log(`Creating ${label} (${url}) in ${client.API_URL}...`);
+
+const { id } = await client.apiFetch("/environment-repository/environments", {
+    label: label,
+    templateId: "browser",
+    nativeConfig: `--disable-background-mode --always-authorize-plugins --allow-outdated-plugins --proxy-server=socks5://127.0.0.1:8090 --window-size=1024,768 ${url}`,
+    driveSettings: [],
+    operatingSystemId: "os:other:chrome-53",
+    enableNetwork: true,
+    enableInternet: true,
+    useWebRTC: true,
+    useXpra: true,
+    xpraEncoding: "jpeg",
+});
+console.log(id);
