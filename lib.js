b9d6b2cd1bddee8122840166ba1f4c2177e377ab
Rafael Gieschke <rafael@gieschke.de>
2023-04-18 15:43:18 +0200

    refactor: move common functions to lib.js, make authentication optional

---
+++ lib.js
@@ -0,0 +1,44 @@
+import { Client } from "./eaas-client/eaas-client.js";
+import { getTokenUsingResourceOwnerPassword } from "./eaas-client/lib/oauth-client.js";
+import { _fetch, Task } from "./eaas-client/lib/util.js";
+
+export const clientFromUrl = async (instanceUrl) => {
+    const url = new URL(instanceUrl);
+    let { username, password } = url;
+    const api = String(
+        Object.assign(new URL("/emil", url), { username: "", password: "" }),
+    );
+    const { id_token } = password
+        ? await getTokenUsingResourceOwnerPassword({
+              issuer: String(new URL("/auth/realms/master", api)),
+              username,
+              password,
+              client_id: "eaas",
+          })
+        : undefined;
+
+    const client = new EaasClient(api, id_token, { emulatorContainer: {} });
+    return client;
+};
+
+class EaasClient extends Client {
+    async apiFetch(path, data, method = "POST") {
+        return await _fetch(
+            `${this.API_URL.replace(/\/+$/, "")}/${path.replace(/^\/+/, "")}`,
+            method,
+            data,
+            this.idToken,
+        );
+    }
+
+    async createTask(path, data) {
+        const { taskId } = await this.apiFetch(path, data);
+        const task = new Task(taskId, this.API_URL, this.idToken);
+        const result = await task.done;
+        if (result.object) {
+            const object = JSON.parse(result.object);
+            return object;
+        }
+        if (result.userData) return result.userData;
+    }
+}
